d8a79e4845d887d56b81e58d349081e47d9b6ee4
gavin3567 <gavinostheimer00@gmail.com>
2023-01-17 22:47:07 -0700

    Add project files.

---
+++ Mission_2_Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission_2_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
